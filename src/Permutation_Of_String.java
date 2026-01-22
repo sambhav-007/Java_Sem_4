aceb1444f96bab3ae7face5d93dd3beb5a69cb5b
sambhav-007 <sambhav.sehgal.007@gmail.com>
2026-01-22 12:36:27 +0530

    This code gives out all the possible permutations of a string and number of permutations

---
+++ src/Permutation_Of_String.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Permutation_Of_String {
+
+    static int count = 0;
+    static void perm(String str, String res) {
+        if (str.isEmpty()) {
+            System.out.println(res);
+            count++;
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String rem = str.substring(0, i) + str.substring(i + 1);
+            perm(rem, ch + res);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a String: ");
+        String str = sc.nextLine();
+        perm(str, "");
+        System.out.println("Total permutations: " + count);
+
+        sc.close();
+    }
+}
