3f996229a3b33e275c2f331dda7651a34d532b28
sambhav-007 <sambhav.sehgal.007@gmail.com>
2026-01-27 12:08:06 +0530

    SubSequence Code Gives out possible permutations of SubSequences of a string

--- src/Permutation_Of_String.java
+++ src/Permutation_Of_String.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
-
 public class Permutation_Of_String {
-
     static int count = 0;
     static void perm(String str, String res) {
         if (str.isEmpty()) {
@@ -9,22 +7,18 @@ public class Permutation_Of_String {
             count++;
             return;
         }
-
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             String rem = str.substring(0, i) + str.substring(i + 1);
             perm(rem, ch + res);
         }
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a String: ");
         String str = sc.nextLine();
         perm(str, "");
         System.out.println("Total permutations: " + count);
-
         sc.close();
     }
 }
---
+++ src/SubSequence.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SubSequence {
+
+    static void subseq(String str, String ans){
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        char c=str.charAt(0);
+        String rest =str.substring(1);
+        subseq(rest,ans);
+        subseq(rest,ans+c);
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Enter String: ");
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        subseq(s,"");
+    }
+}
