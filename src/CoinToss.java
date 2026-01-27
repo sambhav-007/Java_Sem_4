b0fbe590a773662694af667d3bd27b270ab976d6
sambhav-007 <sambhav.sehgal.007@gmail.com>
2026-01-27 12:21:05 +0530

    CoinToss Code Gives out possible permutations of Coin face combinations depending on number of coins tossed

---
+++ src/CoinToss.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CoinToss {
+
+    static void toss(int n, String ans){
+        if(n==0){
+            System.out.println(ans);
+            return;
+        }
+        toss(n-1,ans+"H");
+        toss(n-1,ans+"T");
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter Number of Coins: ");
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        toss(n,"");
+    }
+}
